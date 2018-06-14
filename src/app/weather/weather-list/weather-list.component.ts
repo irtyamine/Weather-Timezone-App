7e5c39f8377110068f7011a8d303fe15fd0148c2
sarwin-github <dapito.sherwin@yahoo.com>
2018-06-14 19:57:24 +0800

    refactor code

--- src/app/weather/weather-list/weather-list.component.ts
+++ src/app/weather/weather-list/weather-list.component.ts
@@ -12,18 +12,26 @@ import { WeatherItem } from "./weather-item";
 export class WeatherListComponent implements OnInit {
 	private req: any;
 
+	items: any;
+
 	constructor(private router:Router, 
 		private activatedRoute: ActivatedRoute,
 		private weatherApiService: WeatherApiService) { }
 
 	ngOnInit() {
+		// initialize Manila, PH as main 
 		this.req = this.weatherApiService.getCurrentWeatherData('Manila', 'PH').subscribe(result => {
-			let weatherItem = new WeatherItem(result.name, result.name, result.name, result.name);
-
+			let weatherItem = new WeatherItem(
+				result.name, 
+				result.sys.country, 
+				result.main.temp, 
+				result.weather[0].main + ', ' + result.weather[0].description
+			);
 			//initial item
 			this.weatherApiService.addWeatherItem(weatherItem);
+			this.items = this.weatherApiService.getWeatherItems();
 
-			console.log(this.weatherApiService.getWeatherItems());
+			console.log(this.items, result);
 		});
 	}
 
